de2a2834e496934bf749e317581efd84e5c9cc11
Ivan Poletsky <wiperus55@gmail.com>
2020-07-25 16:58:03 +0600

    switching to gradle on kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdkVersion(30)
+
+    buildFeatures {
+        dataBinding = true
+    }
+    defaultConfig {
+        applicationId = "ru.omsu.themoviedb"
+        minSdkVersion(29)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    buildToolsVersion = "30.0.1"
+}
+
+
+dependencies {
+    kapt("com.github.bumptech.glide:compiler:4.11.0")
+    kapt("androidx.room:room-compiler:2.2.5")
+
+    kotlin("kotlin-stdlib-jdk8:1.3.72")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.room:room-runtime:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
+    implementation("androidx.fragment:fragment-ktx:1.2.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    implementation("com.android.support:cardview-v7:30.0.0")
+    implementation("com.android.support:design:30.0.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.8.0")
+
+    implementation("org.koin:koin-android:2.1.6")
+    implementation("org.koin:koin-android-viewmodel:2.1.6")
+
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
