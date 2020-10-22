f989f7c87b55ef5065e0863f8a14ffe9cdec45c4
Ivan Poletsky <wiperus55@gmail.com>
2020-10-22 19:55:22 +0600

    Removed useless comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 dependencies {
     implementation(project(":api"))
     kapt("androidx.room:room-compiler:2.2.5")
-    //kotlin("kotlin-stdlib-jdk8:1.3.72")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 
