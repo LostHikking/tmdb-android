67763d9d64ecd654fb41f20269564b021903ed07
Ivan Poletsky <wiperus55@gmail.com>
2020-11-01 14:03:03 +0600

    Corrected DTO fields

--- api/src/main/kotlin/io/github/losthikking/themoviedb/api/dto/movie/Collection.kt
+++ api/src/main/kotlin/io/github/losthikking/themoviedb/api/dto/movie/Collection.kt
@@ -6,11 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Collection(
     @SerialName("backdrop_path")
-    val backdropPath: String?,
+    val backdropPath: String? = null,
     val id: Int,
-    val name: String?,
-    val overview: String?,
-    val parts: List<Movie>,
+    val name: String,
+    val overview: String? = null,
+    val parts: List<Movie> = listOf(),
     @SerialName("poster_path")
     val posterPath: String?
 )
