89912fcc2561ef75cbb10e2ad166ee084ed8ab68
Ivan Poletsky <wiperus55@gmail.com>
2020-11-01 14:03:30 +0600

    Added serialization test

---
+++ api/src/test/kotlin/io/github/losthikking/themoviedb/api/SerializationTest.kt
@@ -0,0 +1,30 @@
+package io.github.losthikking.themoviedb.api
+
+import io.github.losthikking.themoviedb.api.dto.Page
+import io.github.losthikking.themoviedb.api.dto.movie.Movie
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.core.spec.style.StringSpec
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
+
+class SerializationTest : StringSpec({
+    val movieJsonString = javaClass.classLoader.getResource("movie_detail.json")!!.readText()
+
+    "Movie" {
+        shouldNotThrow<SerializationException> {
+            json.decodeFromString<Movie>(movieJsonString)
+        }
+    }
+
+    val popularMoviesJsonString =
+        javaClass.classLoader.getResource("popular_movies.json")!!.readText()
+
+    "Movie Page" {
+        shouldNotThrow<SerializationException> {
+            json.decodeFromString<Page<Movie>>(popularMoviesJsonString)
+        }
+    }
+})
+
+
+
